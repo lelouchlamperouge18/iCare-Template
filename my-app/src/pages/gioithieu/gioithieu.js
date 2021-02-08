d2b12395812245e931e8c80c13160f94b9b4e277
Hoang Cong Son <congsonhoang0810@gmail.com>
2021-02-08 22:01:51 +0700

    add temp gioithieu page

--- my-app/src/pages/gioithieu/gioithieu.js
+++ my-app/src/pages/gioithieu/gioithieu.js
@@ -1,35 +1,119 @@
 import React from 'react';
 import './gioithieu.css'
-import { Container, Row, Col, Alert } from 'reactstrap'
+import history from '../../assets/photos/history.png'
+import { Container, Row, Col } from 'reactstrap'
 
 export function Gioithieu() {
     return (
-        <div>
-            GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>GIOITHIEU PAGE <br></br>
-            GIOITHIEU PAGE <br></br>
+        <div className="Gioithieu">
+            <Container>
+                <Row>
+                    <Col xs="12" sm="12" md="12" className="gioithieu__visionmisson--history">
+                        <div className="gioithieu__title">
+                            HÌNH THÀNH VÀ PHÁT TRIỂN
+                        </div>
+                        <img src={history} className="img-fluid" alt="History of iCare"></img>
+                    </Col>
+                </Row>
+            </Container>
+            <Container fluid className="gioithieu__visionmisson">
+                <Container>
+                <Row>
+                    <Col xs="12" sm="12" md="12">
+                        <div className="gioithieu__title">
+                            TẦM NHÌN VÀ NHIỆM VỤ
+                        </div>
+                    </Col>
+                    <Col xs="12" sm="12" md="6">
+                        <Row>
+                            <Col xs="2" sm="2" md="2" class="gioithieu__visionmisson--block">
+                                TẦM NHÌN
+                            </Col>
+                            <Col xs="10" sm="10" md="10">
+                                <div className="gioithieu__visionmisson--des1">
+                                    Trở thành một trong những nhà sản xuất và cung cấp thuốc chữa bệnh hiệu quả, uy tín nhất trong nước và khu vực.
+                                </div>
+                                <div className="gioithieu__visionmisson--des2">
+                                Lelouch vi Britannia (ルルーシュ・ヴィ・ブリタニア, Rurūshu vui Buritania) is the main protagonist of Code Geass: Lelouch of the Rebellion and Code Geass: Lelouch of the Rebellion R2
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs="2" sm="2" md="2" class="gioithieu__visionmisson--block">
+                                NHIỆM VỤ
+                            </Col>
+                            <Col xs="10" sm="10" md="10">
+                                <Row>
+                                    <Col xs="1" sm="1" md="1" className="gioithieu__visionmisson--des">
+                                        ✔
+                                    </Col>
+                                    <Col xs="11" sm="11" md="11" className="gioithieu__visionmisson--des">
+                                        <div className="gioithieu__visionmisson--des3">
+                                            Cung cấp cho khách hàng những sản phẩm tốt nhất với thời gian đáp ứng ngắn nhất
+                                        </div>
+                                        <div className="gioithieu__visionmisson--des2">
+                                            Lelouch vi Britannia (ルルーシュ・ヴィ・ブリタニア, Rurūshu vui Buritania) is the main protagonist of Code Geass: Lelouch of the Rebellion and Code Geass: Lelouch of the Rebellion R2
+                                        </div>
+                                    </Col>
+                                    <Col xs="1" sm="1" md="1" className="gioithieu__visionmisson--des"> 
+                                        ✔
+                                    </Col>
+                                    <Col xs="11" sm="11" md="11" className="gioithieu__visionmisson--des">
+                                        <div className="gioithieu__visionmisson--des3">
+                                            Không ngừng cải tiến chất lượng sản phẩm và phát triển sản phẩm ở mức cao
+                                        </div>
+                                        <div className="gioithieu__visionmisson--des2">
+                                            Lelouch vi Britannia (ルルーシュ・ヴィ・ブリタニア, Rurūshu vui Buritania) is the main protagonist of Code Geass: Lelouch of the Rebellion and Code Geass: Lelouch of the Rebellion R2
+                                        </div>
+                                    </Col>
+                                    <Col xs="1" sm="1" md="1" className="gioithieu__visionmisson--des">
+                                        ✔
+                                    </Col>
+                                    <Col xs="11" sm="11" md="11" className="gioithieu__visionmisson--des">
+                                        <div className="gioithieu__visionmisson--des3">
+                                            Duy trì và phát triển tốt thị trường trong nước, mở rộng xuất khẩu
+                                        </div>
+                                        <div className="gioithieu__visionmisson--des2">
+                                            Lelouch vi Britannia (ルルーシュ・ヴィ・ブリタニア, Rurūshu vui Buritania) is the main protagonist of Code Geass: Lelouch of the Rebellion and Code Geass: Lelouch of the Rebellion R2
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col xs="12" sm="12" md="6">
+                        <img className="img-fluid" src="https://fantasticmemes.files.wordpress.com/2016/02/zetsuen_17_6.jpg" alt=""></img>
+                    </Col>
+                </Row>
+                </Container>
+            </Container>
+            <Container className="gioithieu__achievement">
+                <Row>
+                    <Col xs="12" sm="12" md="12">
+                        <div className="gioithieu__title">
+                            THÀNH TÍCH
+                        </div>
+                    </Col>                    
+                    <Col xs="12" sm="12" md="4">
+                    <img className="img-fluid" src="https://otakukart.com/wp-content/uploads/2021/01/tokyo.jpg" alt=""></img>
+                    </Col>
+                    <Col xs="12" sm="12" md="8">
+                        <div className="gioithieu__achievement--des1">
+                            Triệu hồi Thuật là một dạng thuật cao cấp đòi hỏi một lượng Chakra khổng lồ từ người sử dụng.
+                            Vật triệu tập thường được gọi ra trong một đống khói với Khẩu Ký Thuật–口寄せの術|Kuchiyose no Jutsu. 
+                            Trước khi có thể triệu tập, người dùng ký một bản giao kèo bằng chính máu của mình, mỗi giao kèo khác nhau dựa theo con vật được triệu tập.
+                        </div>
+                        <div className="gioithieu__achievement--des2">
+                            <ul>
+                                <li>▪ Một khi đã xong, người triệu tập chỉ cần thực hiện chính xác thủ ấn và cho thêm một chút máu từ cùng bàn tay mà họ dùng để ký giao kèo.</li>
+                                <li>▪ Lượng chakra cần thiết lúc này cân xứng với kích cỡ của con vật được triệu tập.</li>
+                                <li>▪ Vật triệu tập có những chiêu thức mà chúng tự sử dụng hoặc dùng kết hợp với người triệu tập. Vài vật triệu tập còn mạnh đến mức thách thức người triệu tập, hay sẽ yêu cầu thứ gì đó để tham gia trận chiến.</li>
+                                <li>▪ Trong vài trường hợp, có thể gọi ra những vật thể bất động, như Orochimaru và Sakon và Ukon làm với cổng 羅生門 - La Sinh Môn(Rashomon) và Tenten với vũ khí đặc biệt. Cũng như với vật triệu tập sống, vật triệu tập bất động cần máu của chính người triệu hồi</li>
+                            </ul>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
